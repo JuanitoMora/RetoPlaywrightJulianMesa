02ba11cb1caeadf1723caa6091ca482ea2b79833
JuanitoMora <juanca.mora89@gmail.com>
2026-06-30 23:46:47 -0500

    editTest

--- tests/training.spec.ts
+++ tests/training.spec.ts
@@ -5,8 +5,8 @@ import {test} from '@playwright/test';
 test('loginSauceDemo', async ({ page }) => {
 
     await page.goto('https://www.saucedemo.com/')
-    await page.getByPlaceholder('Username').fill('standard_user')
-    await page.getByPlaceholder('Password').fill('secret_sauce')
+    await page.getByRole('textbox', { name:'Username'}).fill('standard_user')
+    await page.getByRole('textbox', { name:'Password'}).fill('secret_sauce')
     await page.getByRole('button', { name: 'Login' }).click()
 
 })
