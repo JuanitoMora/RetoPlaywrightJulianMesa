baa7c9bb36be9c62205bb19515f02b71a663685f
JuanitoMora <juanca.mora89@gmail.com>
2026-07-14 22:49:21 -0500

    :sparkles: 'newChallenge2'

--- tests/reto1LogginToHRM.spec.ts
+++ tests/reto1LogginToHRM.spec.ts
@@ -1,44 +1,49 @@
-import { expect, test } from "@playwright/test";
+import { expect, test } from '@playwright/test';
 
-test("logginCorrecto", async ({ page }) => {
-  await page.goto("https://opensource-demo.orangehrmlive.com");
-  await page.getByRole("textbox", { name: "Username" }).fill("Admin");
-  await page.getByRole("textbox", { name: "Password" }).fill("admin123");
-  await page.getByRole("button", { name: "Login" }).click();
+test('logginCorrecto', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
 
-  await expect(page.getByRole("link", { name: "Dashboard" })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
+  await page.close();
 });
 
-test("noLogginConUsuarioIncorrecto", async ({ page }) => {
-  await page.goto("https://opensource-demo.orangehrmlive.com");
-  await page.getByRole("textbox", { name: "Username" }).fill("UsuarioNoValido");
-  await page.getByRole("textbox", { name: "Password" }).fill("admin123");
-  await page.getByRole("button", { name: "Login" }).click();
+test('noLogginConUsuarioIncorrecto', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com');
+  await page.getByRole('textbox', { name: 'Username' }).fill('UsuarioNoValido');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
 
-  await expect(page.getByText("Invalid credentials")).toBeVisible();
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
+  await page.close();
 });
 
-test("noLogginConPasswordIncorrecto", async ({ page }) => {
-  await page.goto("https://opensource-demo.orangehrmlive.com");
-  await page.getByRole("textbox", { name: "Username" }).fill("Admin");
-  await page.getByRole("textbox", { name: "Password" }).fill("admin12345");
-  await page.getByRole("button", { name: "Login" }).click();
+test('noLogginConPasswordIncorrecto', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin12345');
+  await page.getByRole('button', { name: 'Login' }).click();
 
-  await expect(page.getByText("Invalid credentials")).toBeVisible();
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
+  await page.close();
 });
 
-test("noLogginConCaracteresEspeciales", async ({ page }) => {
-  await page.goto("https://opensource-demo.orangehrmlive.com");
-  await page.getByRole("textbox", { name: "Username" }).fill("Admin$%!@");
-  await page.getByRole("textbox", { name: "Password" }).fill("id=1");
-  await page.getByRole("button", { name: "Login" }).click();
+test('noLogginConCaracteresEspeciales', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin$%!@');
+  await page.getByRole('textbox', { name: 'Password' }).fill('id=1');
+  await page.getByRole('button', { name: 'Login' }).click();
 
-  await expect(page.getByText("Invalid credentials")).toBeVisible();
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
+  await page.close();
 });
 
-test("noLogginConCamposObligatoriosVacios", async ({ page }) => {
-  await page.goto("https://opensource-demo.orangehrmlive.com");
-  await page.getByRole("button", { name: "Login" }).click();
+test('noLogginConCamposObligatoriosVacios', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com');
+  await page.getByRole('button', { name: 'Login' }).click();
 
   await expect(page.getByText('Required').first()).toBeVisible();
+  await page.close();
 });
